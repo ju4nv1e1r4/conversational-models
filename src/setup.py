7604602384d5d681aa00b1af80f67f0c22e93fab
ju4nv1e1r4 <juanvieira1@yahoo.com>
2026-01-20 17:56:31 -0300

    feat: impl validation of artifacts dir

--- src/setup.py
+++ src/setup.py
@@ -5,16 +5,23 @@ import os
 sys.path.append(os.getcwd())
 
 from src.services.model_builder import ModelBuilder
+from src.utils.config import settings
 
 logger = logging.getLogger("Entrypoint")
 logging.basicConfig(level=logging.INFO)
 
 MODEL_ID = "Xenova/nli-deberta-v3-xsmall"
+ARTIFACT_NAME = "intent_classifier.zip"
 
 def main():
+    artifact_path = os.path.join(settings.ARTIFACTS_PATH, ARTIFACT_NAME)
+    if os.path.exists(artifact_path):
+        logger.info(f"Modelo já existe em {artifact_path}. Pulando build.")
+        return
+
     builder = ModelBuilder(
         model_id=MODEL_ID,
-        artifact_name="intent_classifier.zip"
+        artifact_name=ARTIFACT_NAME
     )
     logger.info("Rodando builder...")
     if builder.run():

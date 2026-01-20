babd34e4916b163fa3d9a643c23b90bde84a61ca
ju4nv1e1r4 <juanvieira1@yahoo.com>
2026-01-20 17:34:12 -0300

    refactor: solving technical debts and adding more logging
    **debt:** we need this boolean return on run method from model_builder to validate and ensure module is working;
    **logging:** crucial to debug build-time.

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,27 @@
+import logging
+import sys
+import os
+
+sys.path.append(os.getcwd())
+
 from src.services.model_builder import ModelBuilder
+
+logger = logging.getLogger("Entrypoint")
+logging.basicConfig(level=logging.INFO)
 
 MODEL_ID = "Xenova/nli-deberta-v3-xsmall"
 
 def main():
     builder = ModelBuilder(
         model_id=MODEL_ID,
-        artifact_name="intent_classifier_v1.zip"
+        artifact_name="intent_classifier.zip"
     )
-    builder.run()
+    logger.info("Rodando builder...")
+    if builder.run():
+        logger.info("Builder finalizado.")
+    else:
+        logger.error("Builder falhou.")
+
 
 if __name__ == "__main__":
     main()
